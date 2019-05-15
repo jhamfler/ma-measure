b3b67c88e314eb8295d36022767588b854ea915a
Johannes Hamfler <jh@z7k.de>
2019-05-15 13:11:00 +0200

    increase calls to test if load balancer is load dependent

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,13 @@ echo started
 env
 echo ""
 
+if [ -z "$TIMES" ]
+then
+	TIMES=10000
+fi
+
 # call 100 times - load balancer handles the rest
-for i in {1..100}
+for i in {1..$TIMES}
 do
 	curl -v "http://ue.default.svc.cluster.local:55555/" &
 	sleep 0.001
