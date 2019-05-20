2c080df3e6c8ab1b75d18b3a1cf28042fbacc1a7
Johannes Hamfler <jh@z7k.de>
2019-05-20 10:15:05 +0200

    use same svc as pod port to satisfy istio

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,15 @@ then
 	TIMES=1000
 fi
 
+if [ -z "$PORT" ]
+then
+	PORT=4430
+fi
+
 # call x times - load balancer handles the rest
 for i in $(seq 1 $TIMES)
 do
-	curl "http://ue.default.svc.cluster.local:55555/" &
+	curl "http://ue.default.svc.cluster.local:4430/" &
 	pids[${i}]=$!
 	sleep 0.001
 done
